714fdebc85d5c2383921ebd7eecd80007b692640
inhyeok <v7153623@naver.com>
2022-04-27 12:42:16 +0900

    memo commit2

--- src/main/java/com/inhyeok/ex2/entity/Memo.java
+++ src/main/java/com/inhyeok/ex2/entity/Memo.java
@@ -1,15 +1,22 @@
 package com.inhyeok.ex2.entity;
 
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name="tbl_memo")
 @ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Memo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
 }
